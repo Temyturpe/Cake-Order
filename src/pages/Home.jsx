b5ac5ff3491e3ecfc385e045dc536d3004c64512
Temitope <themyturpeokesanya@gmail.com>
2023-01-07 00:04:33 +0100

    featured section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Categories from "../components/categories/Categories";
+import Featured from "../components/featured/Featured";
 import Header from "../components/header/Header";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div className=" px-5 md:px-[120px] mx-auto">
       <Header />
       <Categories />
+      <Featured />
     </div>
   );
 };
