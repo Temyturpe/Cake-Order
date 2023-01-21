1ae1caa44b2f2ecb3efdfa0599283d17d5cc03ba
Temitope <themyturpeokesanya@gmail.com>
2023-01-21 23:12:05 +0100

    menu page

---
+++ src/components/menu/Menuheader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Bakery from '../../assets/images/cakeshop.jpg'
+import './menu.css'
+
+const Menuheader = () => {
+    return (
+        <div className='w-full'>
+            <div className="img h-[200px] w-full relative"><img src={Bakery} alt="" className='w-full h-full object-cover' />
+                <div className="title absolute w-full h-full menubg flex justify-center items-center top-0 left-0 uppercase text-7xl font-semibold text-white">our menu</div></div>
+        </div>
+    )
+}
+
+export default Menuheader
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,25 +3,49 @@ import "./topbar.css";
 import Logo from "../../assets/images/singleicon.PNG";
 import { BsFillCartFill } from "react-icons/bs";
 import { BiMenu } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
   return (
     <div className="main">
       <div className="bg-lite py-3 md:py-5 px-5 md:px-[120px] shadow-md mx-auto fixed top-0 left-0 w-full z-10 font-open">
         <div className="maincontent flex justify-between">
-          <div className="left flex items-center">
-            <div className="icon">
-              <img src={Logo} alt="" className="w-12 h-12 object-cover" />
-            </div>
-            <div className="name text-lg sm:text-xl text-dark">
-              Tee's Bakery
-            </div>
+          <div className="left ">
+            <Link
+              to="/"
+              onClick={() => {
+                window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+              }}
+              className='flex items-center'
+            >
+              <div className="icon">
+                <img src={Logo} alt="" className="w-12 h-12 object-cover" />
+              </div>
+              <div className="name text-lg sm:text-xl text-dark">
+                Tee's Bakery
+              </div>
+            </Link>
+
           </div>
           <div className="right items-center text-dark flex">
             <div className="link mr-12 hidden md:flex">
               <ul className="flex gap-12 ">
-                <li>home</li>
-                <li>menu</li>
+                <li><Link
+                  to="/"
+                  onClick={() => {
+                    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+                  }}
+                >
+                  home
+                </Link></li>
+                <li><Link
+                  to="/menu"
+                  onClick={() => {
+                    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+                  }}
+                >
+                  menu
+                </Link></li>
                 <li>about us</li>
                 <li>contact us</li>
               </ul>
@@ -45,7 +69,7 @@ const Topbar = () => {
           </div>
         </div>
       </div>
-      <div className="empty w-full p-[10px] mt-[70px] md:mt-[87px] bg-mid"></div>
+
     </div>
   );
 };
