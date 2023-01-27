fd9db1156915a43e35643ce2fae96a52f195b96d
Temitope <themyturpeokesanya@gmail.com>
2023-01-27 21:07:47 +0100

    new menu page update

--- src/components/menu/Menuheader.jsx
+++ src/components/menu/Menuheader.jsx
@@ -6,10 +6,9 @@ const Menuheader = () => {
     return (
         <div className="headhead">
             <div className='w-full'>
-                <div className="img h-[z0px] w-full relative"><img src={Bakery} alt="" className='w-full h-full object-cover' />
+                <div className="img h-[200px] w-full relative"><img src={Bakery} alt="" className='w-full h-full object-cover' />
                     <div className="title absolute w-full h-full menubg flex justify-center items-center top-0 left-0 uppercase text-7xl font-semibold text-white">our menu</div></div>
             </div>
-            <div className="menubody mt-20">helloooo</div>
         </div>
     )
 }
--- src/components/menu/Menumain.jsx
+++ src/components/menu/Menumain.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+// import data
+import { menuData } from "./menudata";
+import { menuNav } from "./menudata";
+
+// import components
+import Singlemenu from "./Singlemenu";
 
 const Menumain = () => {
+    const [item, setItem] = useState({ name: "all" });
+    const [menu, setMenu] = useState([]);
+    const [active, setActive] = useState(0);
+
+    useEffect(() => {
+        // get projects based on item
+        if (item.name === "all") {
+            setMenu(menuData);
+        } else {
+            const newMenu = menuData.filter((menu) => {
+                return menu.category.toLowerCase() === item.name;
+            });
+            setMenu(newMenu);
+        }
+    }, [item]);
+
+    const handleClick = (e, index) => {
+        setItem({ name: e.target.textContent.toLowerCase() });
+        setActive(index);
+    };
+
     return (
-        <div>
-
+        <div className="w-full flex gap-12 pt-20 px-5 md:px-[120px] mx-auto">
+            {/* projects nav */}
+            <nav className="mb-12 flex flex-col mx-auto w-[25%]">
+                <ul className="sm:text-2xl text-xl text-mygray font-font-pacifico">
+                    {menuNav.map((item, index) => {
+                        return (
+                            <li
+                                onClick={(e) => {
+                                    handleClick(e, index);
+                                }}
+                                className={`${active === index ? "active" : ""
+                                    } cursor-pointer capitalize m-4`}
+                                key={index}
+                            >
+                                {item.name}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+            {/* projects */}
+            <div className="grid gap-y-12 lg:grid-cols-3 w-[75%] grid-cols-1 sm:grid-cols-2 lg:gap-x-4 lg:gap-y-8 gap-x-6 ">
+                {menu.map((item) => {
+                    return <Singlemenu item={item} key={item.id} />;
+                })}
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Menumain
+export default Menumain;
