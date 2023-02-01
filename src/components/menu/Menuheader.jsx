bcc5f8d85ec9fac6b253949859ae9f9a69b7684d
Temitope <themyturpeokesanya@gmail.com>
2023-02-02 00:09:41 +0100

    menu

--- src/components/menu/Menuheader.jsx
+++ src/components/menu/Menuheader.jsx
@@ -7,7 +7,7 @@ const Menuheader = () => {
         <div className="headhead">
             <div className='w-full'>
                 <div className="img h-[200px] w-full relative"><img src={Bakery} alt="" className='w-full h-full object-cover' />
-                    <div className="title absolute w-full h-full menubg flex justify-center items-center top-0 left-0 uppercase text-7xl font-semibold text-white">our menu</div></div>
+                    <div className="title absolute w-full h-full menubg flex justify-center items-center top-0 left-0 uppercase text-5xl sm:text-6xl md:text-7xl font-semibold text-white">our menu</div></div>
             </div>
         </div>
     )
